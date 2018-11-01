5582bec567e6b9b5652a9c14fad8670809d99e1c
unknown <alex.gmole@gmail.com>
2018-10-31 21:30:13 -0700

    Fix a actividades mostradas en registro de actividad con solicitud previa

--- control/folioComboBox.php
+++ control/folioComboBox.php
@@ -3,7 +3,7 @@ require '../config/connection.php';
 $i=1;
 session_start();
 $matricula=$_SESSION['matricula'];
-$sql="SELECT * FROM alumno a,actividad b, alumnoactividad c where a.Matricula=c.Matricula and b.folio=c.Folio and a.Matricula=$matricula and b.solicitadoPrev=1 and b.estatusSolicitud=2 and c.aprobacionRegistro=0";
+$sql="SELECT * FROM alumno a,actividad b, alumnoactividad c where a.Matricula=c.Matricula and b.folio=c.Folio and a.Matricula=$matricula and b.solicitadoPrev=1 and b.estatusSolicitud=2 and c.aprobacionCoordinador=0 and c.aprobacionRegistro=0";
 $filas = $dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 foreach ($filas as $filas):
 $datos[$i]=$filas['folio'];
