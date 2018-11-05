39da6a49c968bfd784d84cf0c4fa8a8e03ac8ac6
unknown <davidwars12@hotmail.com>
2018-11-05 13:40:35 -0800

    Arreglo dimensiones tablas

--- ModuloAdministrador/views/menu_consultas.php
+++ ModuloAdministrador/views/menu_consultas.php
@@ -3,10 +3,25 @@
     <meta charset="UTF-8">
     <title>Menu</title>
     <link href="../../assets/bootstrap/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../styles/css/menu.css">
+    <link rel="stylesheet" href="../../styles/css/menu.css">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <!-- SCRIPTS -->
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <style media="screen">
+    tbody, thead tr { display: block; }
+     tbody {
+        height: 400px;
+        overflow-y: auto;
+        overflow-x: hidden;
+    }
+     tbody td, thead th {
+        width: 110px;
+    }
+
+     thead th:last-child {
+        width: 126px; /* 140px + 16px scrollbar width */
+    }
+    </style>
 </head>
 
 
