84dc36e7fb6d9553b19dddf239cedb6421f05b20
unknown <alex.gmole@gmail.com>
2018-11-04 22:53:35 -0800

    fix db connection

--- config/connection.php
+++ config/connection.php
@@ -1,19 +1,19 @@
 <?php
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"], 1);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
 
-define('SERVER', 'localhost');
-define('DATABASE', 'sae');
-define('USERNAME', 'root');
-define('PASSWORD', '');
+// define('SERVER', 'localhost');
+// define('DATABASE', 'sae');
+// define('USERNAME', 'root');
+// define('PASSWORD', '');
 
-// define('SERVER', $cleardb_server);
-// define('USERNAME', $cleardb_username);
-// define('PASSWORD', $cleardb_password);
-// define('DATABASE', $cleardb_db);
+define('SERVER', $cleardb_server);
+define('USERNAME', $cleardb_username);
+define('PASSWORD', $cleardb_password);
+define('DATABASE', $cleardb_db);
 
 try {
   $dbh = new PDO(
