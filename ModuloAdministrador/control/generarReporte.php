018a780b0161c1144dbe395e90580c1ce52f6176
unknown <davidwars12@hotmail.com>
2018-11-04 21:00:49 -0800

    Formato de reporte

--- ModuloAdministrador/control/generarReporte.php
+++ ModuloAdministrador/control/generarReporte.php
@@ -17,34 +17,227 @@ $pagina = $excel->getActiveSheet();
 $pagina->setTitle('Actividades');
 
 require '../../config/connection.php';
-$i=2;
-$sql="SELECT * FROM alumno ORDER BY Carrera";
-$stament = $dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
+//Insertar titulos
 $pagina->setCellValue('A1', 'MATRICULA');
 $pagina->setCellValue('B1', 'NOMBRE');
 $pagina->setCellValue('C1', 'CARRERA');
-$pagina->setCellValue('D1', 'VICULACION ');
+$pagina->setCellValue('D1', 'VINCULACION ');
 $pagina->setCellValue('E1', 'CIENTIFICA');
 $pagina->setCellValue('F1', 'DEPORTIVA');
 $pagina->setCellValue('G1', 'SOCIAL');
 $pagina->setCellValue('H1', 'CULTURAL');
 $pagina->setCellValue('I1', 'TOTAL');
+$pagina->setCellValue('J1', 'ACREDITACION');
+$pagina->setCellValue('K1', '%');
 
-$pagina->getStyle('A1:C1')->getFont()->setBold(true);
-$pagina->getStyle('A1:E1')->getFont()->setSize(12);
+//Agregar fuente
+$pagina->getStyle('A1:K1')->getFont()->setBold(true);
+$pagina->getStyle('A1:K1')->getFont()->setSize(12);
+$i=2;
+$count=0;
+
+//Alumnos Admin Inicio
+$sql="SELECT * FROM alumno where Carrera='ADMINISTRACION DE EMPRESAS'";
+$stament = $dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+//Agregar alumnos
 foreach ($stament as $filas):
   $pagina->setCellValue('A'.($i), $filas['Matricula']);
   $NombreCom=$filas['ApePa'].' '.$filas['ApeMa'].' '.$filas['Nombre'];
   $pagina->setCellValue('B'.($i), $NombreCom);
   $pagina->setCellValue('C'.($i), $filas['Carrera']);
+  $pagina->setCellValue('D'.($i), $filas['ActViculacion']);
+  $pagina->setCellValue('E'.($i), $filas['ActCientifica']);
+  $pagina->setCellValue('F'.($i), $filas['ActDeportiva']);
+  $pagina->setCellValue('G'.($i), $filas['ActRespSoci']);
+  $pagina->setCellValue('H'.($i), $filas['ActCult']);
+  $pagina->setCellValue('I'.($i), '=SUM(D'.$i.':H'.$i.')');
+  $pagina->setCellValue('J'.($i), '=IF(I'.$i.'=0,0,1)');
   $i=$i+1;
+  $count=1+$count;
 endforeach;
+  $pagina->setCellValue('C'.($i), 'SUMA:');
+  //Guardar celda ultimo alumno
+  $A=$i-1;
+  //Calcular SUMA actividades
+  $pagina->setCellValue('D'.($i), '=SUM(D2:D'.$A.')');
+  $pagina->setCellValue('E'.($i), '=SUM(E2:E'.$A.')');
+  $pagina->setCellValue('F'.($i), '=SUM(F2:F'.$A.')');
+  $pagina->setCellValue('G'.($i), '=SUM(G2:G'.$A.')');
+  $pagina->setCellValue('H'.($i), '=SUM(H2:H'.$A.')');
+  $pagina->setCellValue('I'.($i), '=SUM(I2:I'.$A.')');
+  $pagina->setCellValue('J'.($i), '=SUM(J2:J'.$A.')');
+  $pagina->setCellValue('K'.($i), '=(J'.$i.'*100)/'.$count);
+//Alumnos Admin Final
 
-  foreach (range('A','C') as $column ):
+
+//Alumnos INFO Inicio
+$sql="SELECT * FROM alumno where Carrera='INFORMATICA'";
+$stament = $dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+$count=0;
+$i=$i+2;
+$E=$i;
+//Insertar titulos
+$pagina->setCellValue('A'.$i, 'MATRICULA');
+$pagina->setCellValue('B'.$i, 'NOMBRE');
+$pagina->setCellValue('C'.$i, 'CARRERA');
+$pagina->setCellValue('D'.$i, 'VINCULACION ');
+$pagina->setCellValue('E'.$i, 'CIENTIFICA');
+$pagina->setCellValue('F'.$i, 'DEPORTIVA');
+$pagina->setCellValue('G'.$i, 'SOCIAL');
+$pagina->setCellValue('H'.$i, 'CULTURAL');
+$pagina->setCellValue('I'.$i, 'TOTAL');
+$pagina->setCellValue('J'.$i, 'ACREDITACION');
+$pagina->setCellValue('K'.$i, '%');
+//Agregar fuente
+$pagina->getStyle('A'.$i.':K'.$i)->getFont()->setBold(true);
+$pagina->getStyle('A'.$i.':K'.$i)->getFont()->setSize(12);
+$i=$i+1;
+
+
+//Agregar alumnos
+foreach ($stament as $filas):
+  $pagina->setCellValue('A'.($i), $filas['Matricula']);
+  $NombreCom=$filas['ApePa'].' '.$filas['ApeMa'].' '.$filas['Nombre'];
+  $pagina->setCellValue('B'.($i), $NombreCom);
+  $pagina->setCellValue('C'.($i), $filas['Carrera']);
+  $pagina->setCellValue('D'.($i), $filas['ActViculacion']);
+  $pagina->setCellValue('E'.($i), $filas['ActCientifica']);
+  $pagina->setCellValue('F'.($i), $filas['ActDeportiva']);
+  $pagina->setCellValue('G'.($i), $filas['ActRespSoci']);
+  $pagina->setCellValue('H'.($i), $filas['ActCult']);
+  $pagina->setCellValue('I'.($i), '=SUM(D'.$i.':H'.$i.')');
+  $pagina->setCellValue('J'.($i), '=IF(I'.$i.'=0,0,1)');
+  $i=$i+1;
+  $count=1+$count;
+endforeach;
+  $pagina->setCellValue('C'.($i), 'SUMA:');
+  //Guardar celda ultimo alumno
+  $A=$i-1;
+  //Calcular SUMA actividades
+  $pagina->setCellValue('D'.($i), '=SUM(D'.$E.':D'.$A.')');
+  $pagina->setCellValue('E'.($i), '=SUM(E'.$E.':E'.$A.')');
+  $pagina->setCellValue('F'.($i), '=SUM(F'.$E.':F'.$A.')');
+  $pagina->setCellValue('G'.($i), '=SUM(G'.$E.':G'.$A.')');
+  $pagina->setCellValue('H'.($i), '=SUM(H'.$E.':H'.$A.')');
+  $pagina->setCellValue('I'.($i), '=SUM(I'.$E.':I'.$A.')');
+  $pagina->setCellValue('J'.($i), '=SUM(J'.$E.':J'.$A.')');
+  $pagina->setCellValue('K'.($i), '=(J'.$i.'*100)/'.$count);
+//Alumnos INFO Final
+
+//Alumnos NEGO-IN Inicio
+$sql="SELECT * FROM alumno where Carrera='NEGOCIOS INTERNACIONALES'";
+$stament = $dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+$count=0;
+$i=$i+2;
+$E=$i;
+//Insertar titulos
+$pagina->setCellValue('A'.$i, 'MATRICULA');
+$pagina->setCellValue('B'.$i, 'NOMBRE');
+$pagina->setCellValue('C'.$i, 'CARRERA');
+$pagina->setCellValue('D'.$i, 'VINCULACION ');
+$pagina->setCellValue('E'.$i, 'CIENTIFICA');
+$pagina->setCellValue('F'.$i, 'DEPORTIVA');
+$pagina->setCellValue('G'.$i, 'SOCIAL');
+$pagina->setCellValue('H'.$i, 'CULTURAL');
+$pagina->setCellValue('I'.$i, 'TOTAL');
+$pagina->setCellValue('J'.$i, 'ACREDITACION');
+$pagina->setCellValue('K'.$i, '%');
+//Agregar fuente
+$pagina->getStyle('A'.$i.':K'.$i)->getFont()->setBold(true);
+$pagina->getStyle('A'.$i.':K'.$i)->getFont()->setSize(12);
+$i=$i+1;
+//Agregar alumnos
+foreach ($stament as $filas):
+  $pagina->setCellValue('A'.($i), $filas['Matricula']);
+  $NombreCom=$filas['ApePa'].' '.$filas['ApeMa'].' '.$filas['Nombre'];
+  $pagina->setCellValue('B'.($i), $NombreCom);
+  $pagina->setCellValue('C'.($i), $filas['Carrera']);
+  $pagina->setCellValue('D'.($i), $filas['ActViculacion']);
+  $pagina->setCellValue('E'.($i), $filas['ActCientifica']);
+  $pagina->setCellValue('F'.($i), $filas['ActDeportiva']);
+  $pagina->setCellValue('G'.($i), $filas['ActRespSoci']);
+  $pagina->setCellValue('H'.($i), $filas['ActCult']);
+  $pagina->setCellValue('I'.($i), '=SUM(D'.$i.':H'.$i.')');
+  $pagina->setCellValue('J'.($i), '=IF(I'.$i.'=0,0,1)');
+  $i=$i+1;
+  $count=1+$count;
+endforeach;
+  $pagina->setCellValue('C'.($i), 'SUMA:');
+  //Guardar celda ultimo alumno
+  $A=$i-1;
+  //Calcular SUMA actividades
+  $pagina->setCellValue('D'.($i), '=SUM(D'.$E.':D'.$A.')');
+  $pagina->setCellValue('E'.($i), '=SUM(E'.$E.':E'.$A.')');
+  $pagina->setCellValue('F'.($i), '=SUM(F'.$E.':F'.$A.')');
+  $pagina->setCellValue('G'.($i), '=SUM(G'.$E.':G'.$A.')');
+  $pagina->setCellValue('H'.($i), '=SUM(H'.$E.':H'.$A.')');
+  $pagina->setCellValue('I'.($i), '=SUM(I'.$E.':I'.$A.')');
+  $pagina->setCellValue('J'.($i), '=SUM(J'.$E.':J'.$A.')');
+  $pagina->setCellValue('K'.($i), '=(J'.$i.'*100)/'.$count);
+//Alumnos NEGO-IN Final
+
+//Alumnos CONTA Inicio
+$sql="SELECT * FROM alumno where Carrera='CONTADURIA'";
+$stament = $dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+$count=0;
+$i=$i+2;
+$E=$i;
+//Insertar titulos
+$pagina->setCellValue('A'.$i, 'MATRICULA');
+$pagina->setCellValue('B'.$i, 'NOMBRE');
+$pagina->setCellValue('C'.$i, 'CARRERA');
+$pagina->setCellValue('D'.$i, 'VINCULACION ');
+$pagina->setCellValue('E'.$i, 'CIENTIFICA');
+$pagina->setCellValue('F'.$i, 'DEPORTIVA');
+$pagina->setCellValue('G'.$i, 'SOCIAL');
+$pagina->setCellValue('H'.$i, 'CULTURAL');
+$pagina->setCellValue('I'.$i, 'TOTAL');
+$pagina->setCellValue('J'.$i, 'ACREDITACION');
+$pagina->setCellValue('K'.$i, '%');
+//Agregar fuente
+$pagina->getStyle('A'.$i.':K'.$i)->getFont()->setBold(true);
+$pagina->getStyle('A'.$i.':K'.$i)->getFont()->setSize(12);
+$i=$i+1;
+
+//Agregar alumnos
+foreach ($stament as $filas):
+  $pagina->setCellValue('A'.($i), $filas['Matricula']);
+  $NombreCom=$filas['ApePa'].' '.$filas['ApeMa'].' '.$filas['Nombre'];
+  $pagina->setCellValue('B'.($i), $NombreCom);
+  $pagina->setCellValue('C'.($i), $filas['Carrera']);
+  $pagina->setCellValue('D'.($i), $filas['ActViculacion']);
+  $pagina->setCellValue('E'.($i), $filas['ActCientifica']);
+  $pagina->setCellValue('F'.($i), $filas['ActDeportiva']);
+  $pagina->setCellValue('G'.($i), $filas['ActRespSoci']);
+  $pagina->setCellValue('H'.($i), $filas['ActCult']);
+  $pagina->setCellValue('I'.($i), '=SUM(D'.$i.':H'.$i.')');
+  $pagina->setCellValue('J'.($i), '=IF(I'.$i.'=0,0,1)');
+  $i=$i+1;
+  $count=1+$count;
+endforeach;
+  $pagina->setCellValue('C'.($i), 'SUMA:');
+  //Guardar celda ultimo alumno
+  $A=$i-1;
+  //Calcular SUMA actividades
+  $pagina->setCellValue('D'.($i), '=SUM(D'.$E.':D'.$A.')');
+  $pagina->setCellValue('E'.($i), '=SUM(E'.$E.':E'.$A.')');
+  $pagina->setCellValue('F'.($i), '=SUM(F'.$E.':F'.$A.')');
+  $pagina->setCellValue('G'.($i), '=SUM(G'.$E.':G'.$A.')');
+  $pagina->setCellValue('H'.($i), '=SUM(H'.$E.':H'.$A.')');
+  $pagina->setCellValue('I'.($i), '=SUM(I'.$E.':I'.$A.')');
+  $pagina->setCellValue('J'.($i), '=SUM(J'.$E.':J'.$A.')');
+  $pagina->setCellValue('K'.($i), '=(J'.$i.'*100)/'.$count);
+//Alumnos CONTA Final
+
+
+//Acomodar celdas
+  foreach (range('A','K') as $column ):
     $pagina->getColumnDimension($column)->setAutoSize(true);
   endforeach;
 
+//Descargar excel
 $objWriter = PHPExcel_IOFactory::createWriter($excel, 'Excel5');
 $objWriter->save('php://output');
  ?>
