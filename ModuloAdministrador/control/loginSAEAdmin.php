8cf1d90b0cd1cbbcfa98da66b0f45f3fe9f7ea97
Alex GD <alex.gmole@gmail.com>
2019-12-12 09:56:29 -0800

    Despliegue de info de Administrador

--- ModuloAdministrador/control/loginSAEAdmin.php
+++ ModuloAdministrador/control/loginSAEAdmin.php
@@ -5,7 +5,7 @@ $numEmp=$_POST['numEmp'];
 $pass=$_POST['contra'];
 $bandera=0;
 if(strlen($numEmp) > 0 && strlen($pass) > 0){
-  $sql="SELECT * FROM coocarrera";
+  $sql="SELECT * FROM coofp";
   $stament = $dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
   foreach ($stament as $filas):
     if($filas['numEmp']==$numEmp && $filas['Contra']==$pass){
