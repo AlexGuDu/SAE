8ab562a8ea4a0160d669332cde15a9388c765087
Alex GD <alex.gmole@gmail.com>
2019-12-12 16:45:14 -0800

    Acomodo despliegue de imagen

--- ModuloAdministrador/views/menu.php
+++ ModuloAdministrador/views/menu.php
@@ -250,11 +250,11 @@
           </div>
          <br>
           <div class="row">
-          <div class="col-xl-2 col-sm-2"> </div>
-            <div class="col-xl-8 col-sm-8">
-                <img id="previewImgEvidence" width="450" height="500">
-            </div>
-          <div class="col-xl-2 col-sm-2"> </div>
+            <div class="col-xl-2 col-sm-2"> </div>
+              <div class="col-xl-8 col-sm-8">
+                  <img id="previewImgEvidence" width="400" height="400">
+              </div>
+            <div class="col-xl-2 col-sm-2"> </div>
           </div> 
           <br>
           <div class="row">
